35c586d61783e282b3958d4472f4301a42627e72
denernun <denernun@gmail.com>
2023-07-03 12:03:08 -0300

    .\

--- ts-patterns/src/patterns/adapter/demo.ts
+++
@@ -1,9 +0,0 @@
-/// <reference path="adapter.ts" />
-namespace AdapterPattern {
-  export namespace Demo {
-    export function show(): void {
-      const adapter: AdapterPattern.Adapter = new AdapterPattern.Adapter();
-      adapter.call();
-    }
-  }
-}
